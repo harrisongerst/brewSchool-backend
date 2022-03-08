2a5e493d7768a192ac353362b5fcba84dc5fd207
harrisongers <hjgerstenlauer@gmail.com>
2022-03-08 10:40:34 -0500

    initial backend structure setup

---
+++ models/index.js
@@ -0,0 +1,4 @@
+//module.exports = {
+    //User: require('./Post'),
+    //Post: require('./User')
+//}
---
+++ server.js
@@ -0,0 +1,40 @@
+// DEPENDENCIES
+
+// get .env variables
+require("dotenv").config();
+const { PORT = 4000, MONGODB_URL } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
+//models
+const models = require("./models");
+
+//db connection
+mongoose.connect(MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+//connection events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+//middleware
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+//routes
+//for testing
+app.get("/", (req, res) => {
+    res.send("hello world");
+  });
+//listener
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
